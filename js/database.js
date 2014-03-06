f87df76421ff94772736edb58e3855cb1e1e6445
unknown <Stefan@.(none)>
2014-03-06 16:15:15 +0100

    before pull

--- js/database.js
+++ js/database.js
@@ -24,7 +24,6 @@ $(document).ready(function() {
 			required : true,
 			maxLength : 100
 		},
-		//Company: { type: "Member", required: true, inverseProperty: "User" },
 		LinkedInUrl : {
 			type : String,
 			required : true,
@@ -163,15 +162,15 @@ $(document).ready(function() {
 	});
 
 	$data.EntityContext.extend("ITForumDatabase", {
-
-		Users : {
-			type : $data.EntitySet,
-			elementType : User
-		},
 		Events : {
 			type : $data.EntitySet,
 			elementType : Event
 		},
+		Users : {
+			type : $data.EntitySet,
+			elementType : User
+		},
+		
 	});
 
 	var itForumDatabase = new ITForumDatabase({
@@ -406,29 +405,9 @@ $(document).ready(function() {
 			itForumDatabase.Events.filter(function(event) {
 				return event.eventid == sessionStorage.selectedId;
 			}).toArray(function(events) {
-				//$("#pageDetailEvent #eventTitle").html(events.join(""));
-				//   alert(events);
 				EventDetails(events);
-				//$('#pageDetailEvent #eventTitle').html('My name is ' + events.attributes ;
-
 			});
 		});
-
-		/*
-
-		itForumDatabase.Events
-		.filter(Event.Id == sessionStorage.selectedId)
-		.toArray( function(events) {
-		$("#pageDetailEvent #eventTitle").html(events.join(""));
-		alert(events)
-		//$('#pageDetailEvent #eventTitle').html('My name is ' + events.attributes ;
-
-		})
-		});
-
-		*/
-
-		//$('#pageDetailEvent #eventTitle').html('My name is ' + sessionStorage.selectedId);
 
 	});
 
